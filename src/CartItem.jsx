12db26cd81b6c1165fdb3ece70699cdd8038b783
Sanjana BIswas <157401970+Sanj3101@users.noreply.github.com>
2025-06-26 20:43:35 +0530

    Integrated Redux Components

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,47 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+      const costNumber = parseFloat(item.cost.substring(1)); // Remove $ and convert to float
+      total += costNumber * item.quantity;
+    });
+  
+    return total.toFixed(2);
+    
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault(); // Prevent default behavior (e.g., form submission or page reload)
+    onContinueShopping(e); // Call parent function to toggle back to plant view
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem({ name: item.name }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name ));
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1)); // remove '$'
+  return (unitPrice * item.quantity).toFixed(2);
   };
 
   return (
@@ -57,7 +77,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
